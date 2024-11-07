e0900bde72c4a1790b8f0974eb8f8a93f3f25d4f
masud132213 <masud132213@gmail.com>
2024-11-08 02:46:42 +0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['res.subscene.best', 'image.tmdb.org'],
+    },
+    async headers() {
+        return [
+            {
+                // সব API রাউটের জন্য CORS হেডার সেট করি
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+                ]
+            }
+        ];
     }
 }
 
