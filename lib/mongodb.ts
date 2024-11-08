daa87910d32ecf95badf9267e4f967d1e6c2f559
masud132213 <masud132213@gmail.com>
2024-11-09 02:11:30 +0600

    Initial commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -8,7 +8,7 @@ declare global {
     } | undefined;
 }
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env');
