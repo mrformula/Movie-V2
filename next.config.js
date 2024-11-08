4d7092eb0787681e8cc1ad06eb32f4c24d644b9e
masud132213 <masud132213@gmail.com>
2024-11-08 12:04:57 +0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -4,26 +4,8 @@ const nextConfig = {
     images: {
         domains: ['res.subscene.best', 'image.tmdb.org'],
     },
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
-                    { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-                ]
-            }
-        ];
-    },
-    trailingSlash: false,
-    async rewrites() {
-        return [
-            {
-                source: '/:path*',
-                destination: '/:path*'
-            }
-        ];
+    experimental: {
+        outputStandalone: true
     }
 }
 
