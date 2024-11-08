6b171107ec33118e4d9e5ab8e7b786ae03a1e832
masud132213 <masud132213@gmail.com>
2024-11-08 10:16:44 +0600

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'gray': {
+                    700: 'rgb(55, 65, 81)',
+                    800: 'rgb(31, 41, 55)',
+                    900: 'rgb(17, 24, 39)',
+                },
+                'blue': {
+                    400: 'rgb(96, 165, 250)',
+                    500: 'rgb(59, 130, 246)',
+                    600: 'rgb(37, 99, 235)',
+                    700: 'rgb(29, 78, 216)',
+                },
+            },
+        },
     },
     plugins: [],
 } 
